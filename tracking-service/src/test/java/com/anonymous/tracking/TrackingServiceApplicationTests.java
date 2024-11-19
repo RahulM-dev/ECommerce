ceed618d0276277ad8239e846a82c70ea5e2e538
Rahul Mishra <mishrarahul9861@gmail.com>
2024-11-19 22:38:54 +0530

    Couple of services are added.

---
+++ tracking-service/src/test/java/com/anonymous/tracking/TrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymous.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
